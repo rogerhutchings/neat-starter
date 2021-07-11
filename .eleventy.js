4f8e6d68741298d68eaf5235c1d4172407a4930a
Roger Hutchings <roger@hutchin.gs>
2021-07-11 21:49:02 +0100

    Reorg layouts, add admin areas, tailwind theme

--- .eleventy.js
+++ .eleventy.js
@@ -60,6 +60,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
+      layouts: "_layouts",
     },
     htmlTemplateEngine: "njk",
   };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: {
     mode: "all",
@@ -6,12 +8,44 @@ module.exports = {
       whitelist: [],
     },
   },
+  darkMode: false,
   theme: {
-    container: {
-      center: true,
+    colors: {
+      gray: {
+        DEFAULT: '#788393',
+        translucent: '#788393cc',
+      },
+      orange: {
+        DEFAULT: '#E86D39',
+        translucent: '#E86D39cc',
+      },
+      white: {
+        DEFAULT: '#ffffff',
+        translucent: '#ffffffcc',
+      },
+      black: {
+        DEFAULT: '#000000',
+        translucent: '#000000cc',
+      },
+      purple: {
+        DEFAULT: '#311a39',
+        translucent: '#311a39cc',
+      },
+      pink: {
+        DEFAULT: '#d566a4',
+        translucent: '#d566a4cc',
+      },
+      silver: {
+        DEFAULT: '#cccccc',
+      },
+    },
+    fontFamily: {
+      sans: ['Titillium Web', ...defaultTheme.fontFamily.sans],
     },
     extend: {
-      colors: {},
+      minHeight: {
+        header: '576px',
+      },
     },
   },
   variants: {},
